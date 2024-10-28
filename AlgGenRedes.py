5974d53e7258a114d7a4328d5186022382149e69
FabioMojica <fabioandremojicaarmaza@gmail.com>
2024-10-28 16:18:52 -0400

    Se definio la funcion de calcular aptitud  para nuestro algoritmo con satisfaccion desviacion y penealizacion

--- AlgGenRedes.py
+++ AlgGenRedes.py
@@ -8,3 +8,10 @@ GENERACIONES = 100
 TASA_CRUCE = 0.8
 TASA_MUTACION = 0.05
 
+def calcular_aptitud(asignacion):
+    demanda = [random.randint(50, 150) for _ in range(NUM_USUARIOS)]
+    satisfaccion = sum(min(a, d) for a, d in zip(asignacion, demanda))
+    desviacion = abs(sum(asignacion) - BANDA_TOTAL)
+    penalizacion = desviacion * 0.1
+    return satisfaccion - penalizacion
+
